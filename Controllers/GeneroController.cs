b4d26446fbef71156dd7e1d5c43a84c4b3541fb0
Pedro Marval <marvalp.dev@gmail.com>
2022-12-16 14:55:28 -0400

    paquetes y controladores

---
+++ Controllers/GeneroController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestCrud.Controllers
+{
+    [Route("[controller]")]
+    public class GeneroController : Controller
+    {
+        private readonly ILogger<GeneroController> _logger;
+
+        public GeneroController(ILogger<GeneroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
